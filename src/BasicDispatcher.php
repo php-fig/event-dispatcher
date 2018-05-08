a41a51b6c2e8169a9998a1d5075d27f61b42d598
Larry Garfield <larry@garfieldtech.com>
2018-05-07 20:17:59 -0500

    Add a before/after ordered dispatcher.

--- src/BasicDispatcher.php
+++ src/BasicDispatcher.php
@@ -12,7 +12,7 @@ class BasicDispatcher implements DispatcherInterface, RegistrationInterface
      */
     protected $listeners = [];
 
-    public function dispatch(EventInterface $event): EventInterface
+    public function dispatch(EventInterface $event) : EventInterface
     {
         $listeners = $this->listeners[get_class($event)] ?? [];
 
@@ -29,29 +29,76 @@ class BasicDispatcher implements DispatcherInterface, RegistrationInterface
         return $event;
     }
 
-    public function addListener(callable $listener, string $type = null): void
+    public function addListener(callable $listener, string $type = null) : string
     {
-        // @todo This assumes type-based registration. We should benchmark this code to see if it's fast enough.
-        if ($type == null) {
-            // This try-catch is only here to keep OCD linters happy about uncaught reflection exceptions.
-            try {
-                // If the handler has no type on its parameter it is invalid.
-                $reflect = new \ReflectionFunction($listener);
-                $params = $reflect->getParameters();
+        $type = $type ?? $this->getTypeForListener($listener);
 
-                $rType =$params[0]->getType();
-                if ($rType == null) {
-                    throw new \InvalidArgumentException('Listeners must declare an object type they can accept.');
-                }
-                $type = $rType->getName();
-            }
-            catch (\ReflectionException $e) {
-                throw new \RuntimeException('Type error registering listener.', 0, $e);
-            }
-        }
+        $id = uniqid();
 
-        $this->listeners[$type][] = $listener;
+        $this->listeners[$type][$id] = $listener;
+
+        return $id;
     }
 
+    public function addListenerBefore(callable $listener, string $id, $type = null) : string
+    {
+        $type = $type ?? $this->getTypeForListener($listener);
 
+        if (!array_key_exists($id, $this->listeners[$type])) {
+            throw new \RuntimeException('No listener to compare to');
+        }
+
+        // @todo There is very likely a better way to do this. I hope, anyway...
+        $keys = array_keys($this->listeners[$type]);
+        $index = array_search($id, $keys);
+        $before = array_slice($this->listeners[$type], 0, $index);
+        $after = array_slice($this->listeners[$type], $index);
+
+        $newId = uniqid();
+
+        $this->listeners[$type] = array_merge($before, [$newId => $listener], $after);
+
+        return $newId;
+    }
+
+    public function addListenerAfter(callable $listener, string $id, $type = null) : string
+    {
+        $type = $type ?? $this->getTypeForListener($listener);
+
+        if (!array_key_exists($id, $this->listeners[$type])) {
+            throw new \RuntimeException('No listener to compare to');
+        }
+
+        // @todo There is very likely a better way to do this. I hope, anyway...
+        $keys = array_keys($this->listeners[$type]);
+        $index = array_search($id, $keys) + 1;
+        $before = array_slice($this->listeners[$type], 0, $index);
+        $after = array_slice($this->listeners[$type], $index);
+
+        $newId = uniqid();
+
+        $this->listeners[$type] = array_merge($before, [$newId => $listener], $after);
+
+        return $newId;
+    }
+
+    protected function getTypeForListener(callable $listener) : string
+    {
+        // @todo This assumes type-based registration. We should benchmark this code to see if it's fast enough.
+        // This try-catch is only here to keep OCD linters happy about uncaught reflection exceptions.
+        try {
+            // If the handler has no type on its parameter it is invalid.
+            $reflect = new \ReflectionFunction($listener);
+            $params = $reflect->getParameters();
+
+            $rType =$params[0]->getType();
+            if ($rType == null) {
+                throw new \InvalidArgumentException('Listeners must declare an object type they can accept.');
+            }
+            return $rType->getName();
+        }
+        catch (\ReflectionException $e) {
+            throw new \RuntimeException('Type error registering listener.', 0, $e);
+        }
+    }
 }
