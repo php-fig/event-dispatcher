2a4c3edbbb3a8d3c60f89e9ba15fd2a270ebc1f5
Larry Garfield <larry@garfieldtech.com>
2018-05-29 15:02:17 -0500

    Factor out listener set object from the integrated dispatcher.

--- src/IntegratedDispatcher.php
+++ src/IntegratedDispatcher.php
@@ -7,10 +7,12 @@ namespace Psr\Event\Dispatcher;
 class IntegratedDispatcher implements DispatcherInterface, BasicRegistrationInterface, ListenerSetInterface
 {
 
-    /**
-     * @var array
-     */
-    protected $listeners = [];
+    protected $listeners;
+
+    public function __construct(ListenerSetInterface $listeners = null)
+    {
+        $this->listeners = $listeners ?? new UnorderedListenerSet();
+    }
 
     public function dispatch(EventInterface $event): EventInterface
     {
@@ -26,43 +28,16 @@ class IntegratedDispatcher implements DispatcherInterface, BasicRegistrationInte
         return $event;
     }
 
-    public function getListenersFor(EventInterface $event) : \Generator
-    {
-        foreach ($this->listeners as $type => $listeners) {
-            if ($event instanceof $type) {
-                yield from $listeners;
-            }
-        }
-    }
-
     public function addListener(callable $listener, string $type = null): void
     {
-        // @todo This assumes type-based registration. We should benchmark this code to see if it's fast enough.
-        $type = $type ?? $this->getParameterType($listener);
-
-        $this->listeners[$type][] = $listener;
+        $this->listeners->addListener($listener, $type);
     }
 
-    protected function getParameterType(callable $listener) : string
+    public function getListenersFor(EventInterface $event) : \Generator
     {
-        // This try-catch is only here to keep OCD linters happy about uncaught reflection exceptions.
-        try {
-            // If the handler has no type on its parameter it is invalid.
-            $reflect = new \ReflectionFunction($listener);
-            $params = $reflect->getParameters();
-
-            $rType =$params[0]->getType();
-            if ($rType == null) {
-                throw new \InvalidArgumentException('Listeners must declare an object type they can accept.');
-            }
-            $type = $rType->getName();
-        }
-        catch (\ReflectionException $e) {
-            throw new \RuntimeException('Type error registering listener.', 0, $e);
-        }
-
-        return $type;
+        yield from $this->listeners->getListenersFor($event);
     }
+
 
 
 }
---
+++ src/UnorderedListenerSet.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Psr\Event\Dispatcher;
+
+
+class UnorderedListenerSet implements ListenerSetInterface, BasicRegistrationInterface
+{
+
+    /**
+     * @var callable[]
+     */
+    protected $listeners;
+
+    public function getListenersFor(EventInterface $event) : \Generator
+    {
+        foreach ($this->listeners as $type => $listeners) {
+            if ($event instanceof $type) {
+                yield from $listeners;
+            }
+        }
+    }
+
+    public function addListener(callable $listener, string $type = null): void
+    {
+        // @todo This assumes type-based registration. We should benchmark this code to see if it's fast enough.
+        $type = $type ?? $this->getParameterType($listener);
+
+        $this->listeners[$type][] = $listener;
+    }
+
+    protected function getParameterType(callable $listener) : string
+    {
+        // This try-catch is only here to keep OCD linters happy about uncaught reflection exceptions.
+        try {
+            // If the handler has no type on its parameter it is invalid.
+            $reflect = new \ReflectionFunction($listener);
+            $params = $reflect->getParameters();
+
+            $rType =$params[0]->getType();
+            if ($rType == null) {
+                throw new \InvalidArgumentException('Listeners must declare an object type they can accept.');
+            }
+            $type = $rType->getName();
+        }
+        catch (\ReflectionException $e) {
+            throw new \RuntimeException('Type error registering listener.', 0, $e);
+        }
+
+        return $type;
+    }
+}
