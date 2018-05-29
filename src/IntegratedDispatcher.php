1a1d2fa773a56fb2d40c520ed6bdaa58a11d3a01
Larry Garfield <larry@garfieldtech.com>
2018-05-29 14:46:44 -0500

    Split derivation logic to its own method.

--- src/IntegratedDispatcher.php
+++ src/IntegratedDispatcher.php
@@ -38,25 +38,30 @@ class IntegratedDispatcher implements DispatcherInterface, BasicRegistrationInte
     public function addListener(callable $listener, string $type = null): void
     {
         // @todo This assumes type-based registration. We should benchmark this code to see if it's fast enough.
-        if ($type == null) {
-            // This try-catch is only here to keep OCD linters happy about uncaught reflection exceptions.
-            try {
-                // If the handler has no type on its parameter it is invalid.
-                $reflect = new \ReflectionFunction($listener);
-                $params = $reflect->getParameters();
-
-                $rType =$params[0]->getType();
-                if ($rType == null) {
-                    throw new \InvalidArgumentException('Listeners must declare an object type they can accept.');
-                }
-                $type = $rType->getName();
-            }
-            catch (\ReflectionException $e) {
-                throw new \RuntimeException('Type error registering listener.', 0, $e);
-            }
-        }
+        $type = $type ?? $this->getParameterType($listener);
 
         $this->listeners[$type][] = $listener;
+    }
+
+    protected function getParameterType(callable $listener) : string
+    {
+        // This try-catch is only here to keep OCD linters happy about uncaught reflection exceptions.
+        try {
+            // If the handler has no type on its parameter it is invalid.
+            $reflect = new \ReflectionFunction($listener);
+            $params = $reflect->getParameters();
+
+            $rType =$params[0]->getType();
+            if ($rType == null) {
+                throw new \InvalidArgumentException('Listeners must declare an object type they can accept.');
+            }
+            $type = $rType->getName();
+        }
+        catch (\ReflectionException $e) {
+            throw new \RuntimeException('Type error registering listener.', 0, $e);
+        }
+
+        return $type;
     }
 
 
