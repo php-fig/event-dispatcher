b0248240169fcbeabb3ea87c536be6c3a8d7e952
Larry Garfield <larry@garfieldtech.com>
2018-06-02 16:47:56 -0400

    Make the parameter detector work for more than just closures and functions.

--- src/ParameterDeriverTrait.php
+++ src/ParameterDeriverTrait.php
@@ -11,9 +11,20 @@ trait ParameterDeriverTrait
         // This try-catch is only here to keep OCD linters happy about uncaught reflection exceptions.
         try {
             // If the handler has no type on its parameter it is invalid.
-            $reflect = new \ReflectionFunction($listener);
-            $params = $reflect->getParameters();
-
+            if (is_string($listener) || $listener instanceof \Closure) {
+                $reflect = new \ReflectionFunction($listener);
+                $params = $reflect->getParameters();
+            }
+            else if (is_array($listener)) {
+                if (is_object($listener[0])) {
+                    $reflect = new \ReflectionObject($listener[0]);
+                    $params = $reflect->getMethod($listener[1])->getParameters();
+                }
+                else if (class_exists($listener[0])) {
+                    $reflect = new \ReflectionClass($listener[0]);
+                    $params = $reflect->getMethod($listener[1])->getParameters();
+                }
+            }
             $rType =$params[0]->getType();
             if ($rType == null) {
                 throw new \InvalidArgumentException('Listeners must declare an object type they can accept.');
