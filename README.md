52388abb766addce7056ecef04aba8cb2ae4d265
Larry Garfield <larry@garfieldtech.com>
2018-12-19 15:52:48 -0600

    Remove split of messages and tasks, so fold the repositories back together.

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 PSR Event Dispatcher
 ====================
 
-This repository holds the base interfaces related to [PSR-14](http://www.php-fig.org/psr/psr-14/).
+This repository holds the interfaces related to [PSR-14](http://www.php-fig.org/psr/psr-14/).
 
 Note that this is not an Event Dispatcher implementation of its own. It is merely interfaces that describe the components of an Event Dispatcher.  See the specification for more details.
-
-Usage
------
-
-We'll certainly need some stuff in here.
--- src/ListenerProviderInterface.php
+++ src/ListenerProviderInterface.php
@@ -9,11 +9,11 @@ namespace Psr\EventDispatcher;
 interface ListenerProviderInterface
 {
     /**
-     * @param EventInterface $event
+     * @param object $event
      *   An event for which to return the relevant listeners.
      * @return iterable[callable]
      *   An iterable (array, iterator, or generator) of callables.  Each
      *   callable MUST be type-compatible with $event.
      */
-    public function getListenersForEvent(EventInterface $event) : iterable;
+    public function getListenersForEvent(object $event) : iterable;
 }
