9676b0a75a363a8e0a260a4595b1c95a07263b42
Larry Garfield <larry@garfieldtech.com>
2018-12-19 17:14:39 -0600

    Missed a rename.

--- src/StoppableEventInterface.php
+++ src/StoppableEventInterface.php
@@ -19,7 +19,7 @@ interface StoppableEventInterface
      * previous listener halted propagation.
      *
      * @return bool
-     *   True if the Task is complete and no further listeners should be called.
+     *   True if the Event is complete and no further listeners should be called.
      *   False to continue calling listeners.
      */
     public function isPropagationStopped() : bool;
