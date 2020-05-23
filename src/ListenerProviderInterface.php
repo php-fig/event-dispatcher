b97806eb70cd83152c5ceeeafeb79187dbee3da1
Christopher Wosinski <christopher.wos@gmail.com>
2020-05-23 13:56:22 +0200

    Return type hint compatible with code analyzers
    Common code analyzers such as PhpStan and Psalm don't understand the notation 
    @return iterable[callable]
    But they work as expected with
    @return iterable<callable>

--- src/ListenerProviderInterface.php
+++ src/ListenerProviderInterface.php
@@ -11,7 +11,7 @@ interface ListenerProviderInterface
     /**
      * @param object $event
      *   An event for which to return the relevant listeners.
-     * @return iterable[callable]
+     * @return iterable<callable>
      *   An iterable (array, iterator, or generator) of callables.  Each
      *   callable MUST be type-compatible with $event.
      */
